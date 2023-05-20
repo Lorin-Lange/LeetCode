bf6419fcb4d91f2ab9b1937873624bc69158b199
Lorin-Lange <lo.-lange@arcor.de>
2023-05-20 21:35:20 +0200

    improved naming of solution 12

--- 12_integer_to_roman/main.cpp
+++ 12_integer_to_roman/main.cpp
@@ -13,55 +13,55 @@ public:
     string intToRoman(int num) {
         int m = num;
         int mod = m % 10;
-        string str10 = decimalsToRoman(mod);
+        string str1 = digitsToRoman(mod);
         m -= mod;
         mod = m % 100;
-        string str100 = dToRoman(mod);
+        string str10 = tensToRoman(mod);
         m -= mod;
         mod = m % 1000;
-        string str1000 = mToRoman(mod);
+        string str100 = hundredsToRoman(mod);
         m -= mod;
         mod = m % 10000;
-        string str10000 = mmToRoman(mod);
-        return str10000 + str1000 + str100 + str10;
+        string str1000 = thousandsToRoman(mod);
+        return str1000 + str100 + str10 + str1;
     }
 
 private:
-    string decimalsToRoman(int i) {
+    string digitsToRoman(int i) {
         switch(i) {
             case 0: return "";
             case 4: return "IV";
             case 5: return "V";
             case 9: return "IX";
-            default: return decimalsToRoman(i - 1) + "I";
+            default: return digitsToRoman(i - 1) + "I";
         }
     }
     
-    string dToRoman(int i) {
+    string tensToRoman(int i) {
         switch(i) {
             case 0: return "";
             case 40: return "XL";
             case 50: return "L";
             case 90: return "XC";
-            default: return dToRoman(i - 10) + "X";
+            default: return tensToRoman(i - 10) + "X";
         }
     }
 
-    string mToRoman(int i) {
+    string hundredsToRoman(int i) {
         switch(i) {
             case 0: return "";
             case 400: return "CD";
             case 500: return "D";
             case 900: return "CM";
-            default: return mToRoman(i - 100) + "C";
+            default: return hundredsToRoman(i - 100) + "C";
         }
     }
 
-    string mmToRoman(int i) {
+    string thousandsToRoman(int i) {
         if (i == 0) {
             return "";
         } else {
-            return mmToRoman(i - 1000) + "M";
+            return thousandsToRoman(i - 1000) + "M";
         }
     }
 };
