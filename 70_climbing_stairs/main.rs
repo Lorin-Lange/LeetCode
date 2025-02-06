af0febe374b0f13acf375ed6f91e2d87712ce368
Lorin-Lange <lo.-lange@arcor.de>
2025-02-07 00:45:27 +0100

    refactored solution of problem 70 in Rust

--- 70_climbing_stairs/main.rs
+++ 70_climbing_stairs/main.rs
@@ -4,6 +4,8 @@
 //            Solution by Lorin Lange             //
 ////////////////////////////////////////////////////
 
+struct Solution;
+
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
         if n <= 2 { n } else {
@@ -16,4 +18,39 @@ impl Solution {
             b
         }
     }
+}
+
+fn main() {
+    println!("70. Climbing Stairs");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_1() {
+        assert_eq!(Solution::climb_stairs(2), 2);
+    }
+
+    #[test]
+    fn case_2() {
+        assert_eq!(Solution::climb_stairs(3), 3);
+    }
+
+    #[test]
+    fn case_3() {
+        assert_eq!(Solution::climb_stairs(5), 8);
+    }
+
+    #[test]
+    fn case_4() {
+        assert_eq!(Solution::climb_stairs(27), 317811);
+    }
+
+    #[test]
+    fn case_5() {
+        assert_eq!(Solution::climb_stairs(45), 1836311903);
+    }
+
 }
